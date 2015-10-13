fc9653a01cbf73b84bface539b52d39de3da2764
tony <tony.gurnick@gmail.com>
2015-10-13 15:21:01 +1300

    generalising for custom elements

--- index.js
+++ index.js
@@ -15,82 +15,50 @@
     POINT1 = new Luna.Math.Point2();
 
 
-
-
     function draw( element, dc ){
-        //console.log("DRAW >>>",element.tagName  )
-        var css = this.getComputedStyle( element );
-        var pcss = null;
-        if ( element.parent ){
-             pcss =this.getComputedStyle( element.parent );
-        }
 
+        var tag = element.tagName;
 
-
-        if ( css ) {
-            var display = css.display;
-
-            if ( display !== "none") {
-
-                var cssunit = new CssUnit( pcss, 16, geometry.width, geometry.height );
-
-                var x = cssunit.convert( "left",css.left )|| 0 ;
-
-                var y = cssunit.convert( "top",css.top  )|| 0;
-                var w = cssunit.convert( "width",css.width )|| geometry.width ;
-                var h = cssunit.convert( "height",css.height )|| geometry.height;
-                var fs = cssunit.convert( "fontSize",css.fontSize )|| 16;
-
-                var bg = cssunit.convert( "backgroundColor",css.backgroundColor ) || {r:0,g:0,b:0,a:0};
-                var color = cssunit.convert( "color",css.color )|| {r:0,g:0,b:0,a:0};
-
-                var text = element.innerText();
-
-
-                // NOTE TO SELF.  text format width should always be the width of
-                // the content box unless the box is less than 0 then it should be infinite?
-                // unless overflow hidden is set - how do browsers do it?
-                var format= new Luna.Gfx.TextFormat( fs );
-
-                var texLayout = new Luna.Gfx.TextLayout( text, format, w, h );
-
-                RECT.setXY( x, y );
-                RECT.setSize( w, h );
-
-                // posindex.set( element, {x:x,y:y,w:w,h:h});
-                console.log("*---->",bg)
-                RGBA.setRed(bg.r);
-                RGBA.setGreen(bg.g);
-                RGBA.setBlue(bg.b);
-                RGBA.setAlpha(bg.a);
-
-                dc.drawBox(RECT, RGBA);
-
-
-                if ( color ){
-                    RGBA.setRed( color.r );
-                    RGBA.setGreen( color.g );
-                    RGBA.setBlue( color.b );
-                    RGBA.setAlpha( color.a );
-                    texLayout.setColor(RGBA);
-                }
-
-                POINT1.setX(x);
-                POINT1.setY(y);
-
-                dc.drawText( texLayout, POINT1 );
-
-            }
+        if ( tag in this.plugins){
+            this.plugins[ tag ]( element, dc, this.getComputedStyle(element));
         }
 
     }
 
     EtchStrategy.prototype.getComputedStyle = function( el ) {
+
+        /*
+        *   return default css
+        */
         if ( el === null ){
             return new StringUtil("font-size:16px;background-color:black;color:white;width:1920px;height:1080px;").parseCss();
         }
 
-        return el.style
+        var css = el.style;
+        var pcss = null;
+
+        if ( el.parent ){
+            pcss = el.parent.style;
+        }
+
+        // ensure some basic defaults
+        if ( !el.style.backgroundColor ){
+            el.style.backgroundColor="black";
+        }
+        if ( !el.style.color ){
+            el.style.color="white";
+        }
+        var cssunit = new CssUnit(pcss, 16, geometry.width, geometry.height);
+
+        css.left = cssunit.convert("left", css.left) || 0;
+        css.top = cssunit.convert("top", css.top) || 0;
+        css.width = cssunit.convert("width", css.width) || geometry.width;
+        css.height = cssunit.convert("height", css.height) || geometry.height;
+        css.fontSize = cssunit.convert("fontSize", css.fontSize) || 16;
+        css.backgroundColor = cssunit.convert("backgroundColor", css.backgroundColor) || {r: 0, g: 0, b: 0, a: 0};
+        css.color = cssunit.convert("color", css.color) || {r: 0, g: 0, b: 0, a: 0};
+
+        return css;
     };
 
     function EtchStrategy() {
@@ -155,11 +123,50 @@
 
     EtchStrategy.prototype.draw = function( e  ) {
         var element = e.target,
-        dc = this.seed( element );
+            dc = this.seed( element );
 
         dc.beginDraw();
         draw.call( this, element, dc  );
         dc.endDraw();
+    };
+
+    EtchStrategy.prototype.plugins = {
+        "DIV": function( el, dc, css ){
+            var text = el.innerText();
+
+            // NOTE TO SELF.  text format width should always be the width of
+            // the content box unless the box is less than 0 then it should be infinite?
+            // unless overflow hidden is set - how do browsers do it?
+            var format= new Luna.Gfx.TextFormat( css.fontSize );
+            var texLayout = new Luna.Gfx.TextLayout( text, format, css.width, css.height );
+
+            RECT.setXY( css.left, css.top );
+            RECT.setSize( css.width, css.height );
+
+            // posindex.set( element, {x:x,y:y,w:w,h:h});
+
+            RGBA.setRed(   css.backgroundColor.r);
+            RGBA.setGreen( css.backgroundColor.g);
+            RGBA.setBlue(  css.backgroundColor.b);
+            RGBA.setAlpha( css.backgroundColor.a);
+
+            dc.drawBox(RECT, RGBA);
+
+
+            RGBA.setRed(   css.color.r );
+            RGBA.setGreen( css.color.g );
+            RGBA.setBlue(  css.color.b );
+            RGBA.setAlpha( css.color.a );
+
+            texLayout.setColor(RGBA);
+
+            POINT1.setX( css.left );
+            POINT1.setY( css.top );
+
+            dc.drawText( texLayout, POINT1 );
+
+
+        }
     };
 
     module.exports = EtchStrategy;
